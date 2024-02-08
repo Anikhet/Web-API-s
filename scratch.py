2626b1cb304bd44c5b83cb850fedaa0892bcd348
Anikhet Mulky <am9559@g.rit.edu>
2024-02-08 16:04:03 -0500

    update

--- scratch.py
+++ scratch.py
@@ -5,20 +5,36 @@ from zeep import Client
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST' and 'animal_name' in request.form:
-        animal_name = request.form['animal_name'].lower()
-        animal_info_url = f'https://api.api-ninjas.com/v1/animals?name={animal_name}'
-        animal_response = requests.get(api_url, headers={'X-Api-Key': 'g6xtlmtdodBhSqZ0MP4hrA==1s79fFkgQcdxm3nt'})
+    if request.method == 'POST' and 'celebrity_name' in request.form:
+        celebrity_name = request.form['celebrity_name'].lower()
+        celebrity_info_url = f'https://api.api-ninjas.com/v1/celebrity?name={celebrity_name}'
+        celebrity_response = requests.get(celebrity_info_url, headers={'X-Api-Key': 'g6xtlmtdodBhSqZ0MP4hrA==1s79fFkgQcdxm3nt'})
 
-        if animal_response.status_code == 200:
-            animal_data = animal_response.json()[0]  # Assuming the animal data is in the first index
-            location = animal_data.get('location')
-
+        if celebrity_response.status_code == 200:
+            celebrity_data = celebrity_response.json()[0]['name'].title()
+            location = celebrity_response.json()[0]['nationality'] 
             # Now use the location to get the country name
-            country_name = get_country_from_location(location)
-            return render_template('animal.html', animal_name=animal_name, location=location, country_name=country_name)
+            wsdl = 'http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL'
+            client = Client(wsdl=wsdl)
+            result = client.service.FullCountryInfo(location.upper())
+
+            url = "https://search-celebrity-biography.p.rapidapi.com/search/miley%20cyrus"
+
+            headers = {"X-RapidAPI-Key": "7476ee983dmshf63250f46fec516p11287fjsn873e15c8eda0",
+                       "X-RapidAPI-Host": "search-celebrity-biography.p.rapidapi.com"}
+
+            new_response = requests.get(url, headers=headers)
+            birth_date = age = new_response.json()['moreFacts'][1]['value']
+            age = new_response.json()['moreFacts'][2]['value']
+            birth_loc = new_response.json()['moreFacts'][21]['value']
+            nationality = new_response.json()['moreFacts'][22]['value']
+
+            
+            country_flag = result['sCountryFlag']
+            return render_template('index.html', celebrity_name=celebrity_data, country_flag=country_flag,
+                                     birth_date = birth_date, age=age)
         else:
-            return render_template('index.html', error='Failed to fetch animal data.')
+            return render_template('index.html', error='Failed to fetch celebrity data.')
 
     return render_template('index.html')
 
@@ -33,4 +49,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000,debug=True)
